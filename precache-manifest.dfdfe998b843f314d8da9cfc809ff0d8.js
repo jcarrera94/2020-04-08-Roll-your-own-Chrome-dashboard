f50389674569ed01cfaf5162017f9d9e88f97d70
Juan Carrera <juankarrera@gmail.com>
2020-04-08 15:27:59 -0400

    Updates

---
+++ precache-manifest.dfdfe998b843f314d8da9cfc809ff0d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914358b1cbd5fbbd8a97847123a757da",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/index.html"
+  },
+  {
+    "revision": "47234a1cca6b953c857f",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/static/css/main.83abb10b.chunk.css"
+  },
+  {
+    "revision": "0d8e0af71cec17ece521",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/static/js/2.47288bf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/static/js/2.47288bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47234a1cca6b953c857f",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/static/js/main.2ad4f097.chunk.js"
+  },
+  {
+    "revision": "ab198efd100fd6d1e66a",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/static/js/runtime-main.d83170aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2020-04-08-Roll-your-own-Chrome-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
